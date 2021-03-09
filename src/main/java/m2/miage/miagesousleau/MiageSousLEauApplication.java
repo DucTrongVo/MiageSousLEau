e29b2c64869c2976a1b6fe8d9f0f8cf95020d26a
DucTrongVo <duc-trong.vo@toulouse.miage.fr>
2021-03-09 11:11:35 +0100

    first commit

---
+++ src/main/java/m2/miage/miagesousleau/MiageSousLEauApplication.java
@@ -0,0 +1,13 @@
+package m2.miage.miagesousleau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiageSousLEauApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiageSousLEauApplication.class, args);
+    }
+
+}
---
+++ src/test/java/m2/miage/miagesousleau/MiageSousLEauApplicationTests.java
@@ -0,0 +1,13 @@
+package m2.miage.miagesousleau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiageSousLEauApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
